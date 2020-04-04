e94ff486bb005fd0658636ee11d28e4d327b827a
Leandro Collares <leco@uvic.ca>
2020-04-03 21:15:08 -0300

    Updates

---
+++ precache-manifest.57db1fabdee2393ebf5feb8cb3f0bb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99600dcbc2b560142a6a81fc624a3d97",
+    "url": "/beyond-the-3-point-arc/index.html"
+  },
+  {
+    "revision": "99435ff7630dbd3d7bb4",
+    "url": "/beyond-the-3-point-arc/static/css/main.d4163297.chunk.css"
+  },
+  {
+    "revision": "ad7c64461d5a71c67327",
+    "url": "/beyond-the-3-point-arc/static/js/2.41387beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beyond-the-3-point-arc/static/js/2.41387beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99435ff7630dbd3d7bb4",
+    "url": "/beyond-the-3-point-arc/static/js/main.98ead487.chunk.js"
+  },
+  {
+    "revision": "25a2924bfc7d27302368",
+    "url": "/beyond-the-3-point-arc/static/js/runtime-main.3b89ec62.js"
+  }
+]);
